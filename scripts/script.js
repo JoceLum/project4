88903ae21355474ed990afa782446008121ef454
Jocelyn Lum <joce.lum@gmail.com>
2017-08-27 22:25:43 -0400

    added message to play videos for tablt & mobile mode

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ let showApp = {};
 showApp.key = "bd9f6a5c409c05ff7938f1d2d7cae63e";
 
 showApp.getShows = function(show) {
+    $('#warning').empty();
     //first ajax call to retrieve TV show that's submitted by user from the api's database
     $.ajax({
         url: `https://api.themoviedb.org/3/search/tv?api_key=${showApp.key}&query=${show}`,
@@ -26,8 +27,8 @@ showApp.getShows = function(show) {
             }).then(function(res) {
                 //res will be the list of shows that have same genre of the TV show that's submitted by the user
                 var results = res.results;
-                console.log(results);
                 showApp.displayShows(results);
+                console.log(results);
             });
 
         } else {
@@ -37,6 +38,7 @@ showApp.getShows = function(show) {
 }
 
 showApp.displayShows = function(showData) {
+    $('#playMsg').text('Tap on the posters to view videos');
     //to empty show container when user doing a search on another show without refreshing page:
     $('#shows').empty();
     showData.forEach(function(show) {
@@ -45,7 +47,6 @@ showApp.displayShows = function(showData) {
             let titles = $('<h3>').text(show.name);
             //image path for the show's poster
             let imageUrl = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${show.poster_path}`;
-            let secondImageUrl = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${show.backdrop_path}`;
             //bind image path and show ID to image tag of show poster
             let poster = $('<img>').attr('src', imageUrl).attr('dataId', show.id).addClass('poster');
             let playIcon = $('<img>').attr('src', 'assets/play_button.png').addClass('playButton');
@@ -53,8 +54,8 @@ showApp.displayShows = function(showData) {
             let showContainer = $('<div>').addClass('show').append(poster, playIcon, titles);
             //append containers to section with id of shows
             $('#shows').append(showContainer);
-            //show new search button
-            $('#restart').css('display', 'block');
+            //show new search button below results
+            $('#restart').show();
         }
     })
 };
@@ -65,9 +66,9 @@ showApp.events = function() {
         e.preventDefault();
         //store the value of the selected element
         //store input if there is something, and scroll to results section; otherwise, let user know that they need to submit something
-        let usersChoice = $('#show').val();
-        if (usersChoice !== "") {
-            showApp.getShows(usersChoice);
+        let usersInput = $('#show').val();
+        if (usersInput !== "") {
+            showApp.getShows(usersInput);
             $('html, body').animate({
                 scrollTop: $("#shows").offset().top
             }, 1000);
@@ -79,19 +80,17 @@ showApp.events = function() {
 
     //show play button upon hovering over show poster
     $('#shows').on('mouseenter', '.poster', function() {
-        let thisPoster = $(this).parent();
-        thisPoster.find('.playButton').show();
+        let posterWrapper = $(this).parent();
+        posterWrapper.find('.playButton').show();
     }).on('mouseleave', '.poster', function() {
-        console.log('mouseout')
-        let thisPoster = $(this).parent();
-        thisPoster.find('.playButton').hide();
+        let posterWrapper = $(this).parent();
+        posterWrapper.find('.playButton').hide();
     });
 
-    //when user clicks on TV show poster, will bring up modal with trailer for show
+    //when user clicks on TV show poster, will open up lightbox with trailer for show
     $('#shows').on('click', '.show', function() {
         //store TV show id in a variable to be passed into ajax call 
         let singleUrl = $(this).find('.poster').attr("dataId");
-        // console.log(singleUrl);
         $.ajax({
             url: `https://api.themoviedb.org/3/tv/${singleUrl}/videos?api_key=${showApp.key}`,
             method: 'GET',
@@ -103,7 +102,7 @@ showApp.events = function() {
         }).then(function(res) {
             //after ajax call is complete, store value of the key in a variable called videos
             let videos = res.results;
-            //only return videos with the type "Trailer or Opening Credits, otherwise display error that there are no videos
+            //first, return videos with the type "Trailer; if there are no trailers, play other videos; if no videos, display error that there are no videos
             var videoKey;
             //to narrow down to shows that have videos 
             if (videos.length !== 0) {
@@ -131,9 +130,11 @@ showApp.events = function() {
             lity(`//www.youtube.com/watch?v=${videoKey}`)
         });
     });
+    //if want to do another search, refresh page and scroll back to top
     $('#restart').on('click', function(e) {
         e.preventDefault();
         location.reload();
+        $(window).scrollTop(0);
     })
 
 }
